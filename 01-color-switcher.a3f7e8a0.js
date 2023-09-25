f465b7df167fac290ef3a31d9aa9ef2362ca1e5c
Jszir <123523194+Jszir@users.noreply.github.com>
2023-09-25 14:37:41 +0000

    Deploying to gh-pages from @ Jszir/goit-js-hw-09@c4b5537541b9411fba605c4dc76ab3336160cce8 🚀

---
+++ 01-color-switcher.a3f7e8a0.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.body,a=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");a.addEventListener("click",(function(n){a.disabled=!0,d.disabled=!1,t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));e.style.backgroundColor=t}),1e3)})),d.addEventListener("click",(function(e){a.disabled=!1,d.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.a3f7e8a0.js.map
